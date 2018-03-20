c45478b96b1d8c41bfae563d3a3d97ab0ddab396
Eric Tobin <eatobin@gmail.com>
2018-03-20 17:26:03 -0500

    tests pass

--- main/main_test.go
+++ main/main_test.go
@@ -4,16 +4,17 @@ import (
 	"testing"
 )
 
-// var brsJ = readFileIntoJsonString("borrowers-before.json")
-// var bksJ = readFileIntoJsonString("books-before.json")
+var brsJ = "[\n  {\n    \"name\": \"Borrower100\",\n    \"max-books\": 100\n  },\n  {\n    \"name\": \"Borrower200\",\n    \"max-books\": 200\n  }\n]\n"
+var bksJ = "[\n  {\n    \"title\": \"Book100\",\n    \"author\": \"Author100\",\n    \"borrower\": {\n      \"name\": \"Borrower100\",\n      \"max-books\": 100\n    }\n  },\n  {\n    \"title\": \"Book200\",\n    \"author\": \"Author200\",\n    \"borrower\": {\n      \"name\": \"NoName\",\n      \"max-books\": -1\n    }\n  }\n]\n"
 
 func TestReadFileIntoJSONString(t *testing.T) {
 	cases := []struct {
 		f    string
 		want string
 	}{
-		{"borrowers-before.json", ""},
-		// 		{brs1, br2lib, brs1},
+		{"../borrowers-before.json", brsJ},
+		{"../books-before.json", bksJ},
+		{"../NoSuch.json", ""},
 	}
 	for _, c := range cases {
 		got := ReadFileIntoJsonString(c.f)
