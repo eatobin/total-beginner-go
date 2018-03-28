ac1c3ec5b051640bb8871443d3bcb20d27962bd0
Eric Tobin <eatobin@gmail.com>
2018-03-28 17:41:34 -0500

    main progress

--- main/main.go
+++ main/main.go
@@ -5,8 +5,13 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/eatobin/totalbeginnergo/book"
+	"github.com/eatobin/totalbeginnergo/borrower"
 	"github.com/eatobin/totalbeginnergo/library"
 )
+
+var tvBorrowers []borrower.Borrower
+var tvBooks []book.Book
 
 func ReadFileIntoJsonString(f string) string {
 	raw, err := ioutil.ReadFile(f)
@@ -27,13 +32,22 @@ func WriteJSONStringToFile(js string, fp string) {
 }
 
 func main() {
-	brsJ := ReadFileIntoJsonString("borrowers-before.json")
-	fmt.Printf("%q\n", brsJ)
-	bksJ := ReadFileIntoJsonString("books-before.json")
-	fmt.Printf("%q\n", bksJ)
-	brs := library.JSONStringToBorrowers(brsJ)
-	bks := library.JSONStringToBooks(bksJ)
-	nBks := library.CheckOut("Borrower200", "Book200", brs, bks)
-	nBksJ := library.BooksToJSONSting(nBks)
-	WriteJSONStringToFile(nBksJ, "books-after.json")
+	tvBorrowers = append(tvBorrowers, borrower.MakeBorrower("Jim", 3))
+	tvBorrowers = append(tvBorrowers, borrower.MakeBorrower("Sue", 3))
+	tvBooks = append(tvBooks, book.MakeBook("War And Peace", "Tolstoy"))
+	tvBooks = append(tvBooks, book.MakeBook("Great Expectations", "Dickens"))
+	println("\nJust created new library")
+	println(library.StatusToString(tvBooks, tvBorrowers))
 }
+
+//func main() {
+//	brsJ := ReadFileIntoJsonString("borrowers-before.json")
+//	fmt.Printf("%q\n", brsJ)
+//	bksJ := ReadFileIntoJsonString("books-before.json")
+//	fmt.Printf("%q\n", bksJ)
+//	brs := library.JSONStringToBorrowers(brsJ)
+//	bks := library.JSONStringToBooks(bksJ)
+//	nBks := library.CheckOut("Borrower200", "Book200", brs, bks)
+//	nBksJ := library.BooksToJSONSting(nBks)
+//	WriteJSONStringToFile(nBksJ, "books-after.json")
+//}
