d42efd2c68db33576d6f0f059406efa1bde14c57
Eric Tobin <eatobin@gmail.com>
2019-01-07 15:53:42 -0600

    get methods for borrower

--- borrower/borrower.go
+++ borrower/borrower.go
@@ -16,10 +16,18 @@ func NewBorrower(n string, mb int) Borrower {
 	return br
 }
 
+func (br Borrower) GetName() string {
+	return br.Name
+}
+
 // SetName sets a Name for a Borrower
 func (br Borrower) SetName(n string) Borrower {
 	br.Name = n
 	return br
+}
+
+func (br Borrower) GetMaxBooks() int {
+	return br.MaxBooks
 }
 
 // SetMaxBooks sets a max books for a Borrower
