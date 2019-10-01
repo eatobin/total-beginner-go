7d63f334964c976454bada6659fc5450d05b1b57
Eric Tobin <eatobin@gmail.com>
2019-09-30 21:27:41 -0500

    all working!

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
-import "fmt"
-
-var br1libPtrX = NewBorrower("Borrower1", 1)
-
 func main() {
-	var runes []rune
-	var runes2 = []rune("Hi")
-	for _, r := range "Hello" {
-		runes = append(runes, r)
-	}
 	var borrowers []*Borrower
 	var books []*Book
 
-	AddBorrower(borrowers, br1libPtrX)
-	fmt.Printf("%q\n", runes)
-	fmt.Printf("%q\n", runes2)
-	//AddBorrower(borrowers, NewBorrower("Sue", 3))
-	AddBook(books, NewBook("War And Peace", "Tolstoy"))
-	//AddBook(books, NewBook("Great Expectations", "Dickens"))
-	//println("\nJust created new library")
+	borrowers = AddBorrower(borrowers, NewBorrower("Jim", 3))
+	borrowers = AddBorrower(borrowers, NewBorrower("Sue", 3))
+	books = AddBook(books, NewBook("War And Peace", "Tolstoy"))
+	books = AddBook(books, NewBook("Great Expectations", "Dickens"))
+	println("\nJust created new library")
 	println(StatusToString(books, borrowers))
 }
