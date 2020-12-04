9753896e3fe8c27bc96b8495d569ca7870d20c22
Eric Tobin <eatobin@gmail.com>
2020-12-04 17:25:23 -0600

    dev with functional borrower - even better

--- borrower/borrower_test.go
+++ borrower/borrower_test.go
@@ -2,25 +2,17 @@ package borrower
 
 import "testing"
 
-var br1 = NewBorrower("Borrower1", 1)
 var wantS = "Borrower1 (1 books)"
-
-func TestBrToString(t *testing.T) {
-	gotS := BrToString(br1)
-	if gotS != wantS {
-		t.Fatalf("BrToString(%v) == %v, want %v", br1, gotS, wantS)
-	}
-}
 
 func TestSetBorrowerValues(t *testing.T) {
 	n := "Borrower1"
-	badBrN := Borrower{"Jack", 1}
+	badBrN := NewBorrower("Jack", 1)
 	gotBrN := BrToString(SetName(badBrN, n))
 	if gotBrN != wantS {
 		t.Fatalf("SetName(%v, %v) == %v, want %v", badBrN, n, gotBrN, wantS)
 	}
 	mb := 1
-	badBrMB := Borrower{"Borrower1", 11}
+	badBrMB := NewBorrower("Borrower1", 11)
 	gotBrMB := BrToString(SetMaxBooks(badBrMB, mb))
 	if gotBrMB != wantS {
 		t.Fatalf("SetMaxBooks(%v, %v) == %v, want %v", badBrMB, mb, gotBrMB, wantS)
