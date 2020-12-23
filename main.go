e67f76052646397c66f0113bcfa95d988118bd6e
Eric Tobin <eatobin@gmail.com>
2020-12-23 14:58:03 -0600

    all working

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-//
-//func main() {
-//	var borrowers []*Borrower
-//	var books []*Book
-//
-//	borrowers = AddBorrower(borrowers, NewBorrower("Jim", 3))
-//	borrowers = AddBorrower(borrowers, NewBorrower("Sue", 3))
-//	books = AddBook(books, NewBook("War And Peace", "Tolstoy"))
-//	books = AddBook(books, NewBook("Great Expectations", "Dickens"))
-//	println("\nJust created new library")
-//	println(StatusToString(books, borrowers))
-//}
+func main() {
+	var borrowers []*Borrower
+	var books []*Book
+
+	borrowers = AddBorrower(borrowers, NewBorrower("Jim", 3))
+	borrowers = AddBorrower(borrowers, NewBorrower("Sue", 3))
+	books = AddBook(books, NewBook("War And Peace", "Tolstoy"))
+	books = AddBook(books, NewBook("Great Expectations", "Dickens"))
+	println("\nJust created new library")
+	println(StatusToString(books, borrowers))
+}
