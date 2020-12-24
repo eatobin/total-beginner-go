50cd910b4e88c0d7668e5c7aaf6785fb76bc862f
Eric Tobin <eatobin@gmail.com>
2020-12-24 14:42:23 -0600

    main progress

--- main.go
+++ main.go
@@ -10,4 +10,14 @@ func main() {
 	books = AddBook(books, NewBook("Great Expectations", "Dickens"))
 	println("\nJust created new library")
 	println(StatusToString(books, borrowers))
+
+	println("Check out War And Peace to Sue")
+	CheckOut("Sue", "War And Peace", borrowers, books)
+	println(StatusToString(books, borrowers))
+
+	println("Now check in War And Peace from Sue...")
+	CheckIn("War And Peace", books)
+	println("...and check out Great Expectations to Jim")
+	CheckOut("Jim", "Great Expectations", borrowers, books)
+	println(StatusToString(books, borrowers))
 }
