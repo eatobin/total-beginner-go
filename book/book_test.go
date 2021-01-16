f74d689513e032e7b476b754a3d2ca1e76e65f8b
Eric Tobin <eatobin@gmail.com>
2021-01-16 14:44:59 -0600

    book test progress

--- book/book_test.go
+++ book/book_test.go
@@ -6,11 +6,9 @@ import (
 )
 
 var jsonStringBk1 = "{\"title\":\"Title11\",\"author\":\"Author11\",\"borrower\":null}"
-var jsonStringBk2 = "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}"
-var br2Ptr = borrower.NewBorrower("Borrower2", 2)
+var jsonStringBk2 = "{\"title\":\"Title11\",\"author\":\"Author11\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}"
 var wantAvailS1 = "Title1 by Author11; Available"
 var wantAvailS2 = "Title11 by Author1; Available"
-var wantAvailS3 = "Title1 by Author1; Checked out to Borrower2"
 
 func TestSetBookValues(t *testing.T) {
 	badBkAvail, _ := JsonStringToBook(jsonStringBk1)
@@ -24,10 +22,11 @@ func TestSetBookValues(t *testing.T) {
 	if gotBkA != wantAvailS2 {
 		t.Fatalf("SetAuthor(%v, %v) == %v, want %v", badBkAvail, author, gotBkA, wantAvailS2)
 	}
-	//newBorrower := borrower.NewBorrower("Borrower2", 2)
-	//badBkB := Book{"Title1", "Author1", borrower.ZeroBorrower}
-	//gotBkB := String(SetBorrower(badBkB, newBorrower))
-	//if gotBkB != wantNotAvailS {
-	//	t.Fatalf("SetBorrower(%v, %v) == %v, want %v", badBkB, newBorrower, gotBkB, wantNotAvailS)
-	//}
+	bkNotAvail, _ := JsonStringToBook(jsonStringBk2)
+	wantNotAvailS := String(bkNotAvail)
+	br2 := borrower.NewBorrower("Borrower2", 2)
+	gotBkB := String(SetBorrower(badBkAvail, br2))
+	if gotBkB != wantNotAvailS {
+		t.Fatalf("SetBorrower(%v, %v) == %v, want %v", badBkAvail, br2, gotBkB, wantNotAvailS)
+	}
 }
