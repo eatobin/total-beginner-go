a27d05c3bbd7237b5512f72c297051a57fa70289
Eric Tobin <eatobin@gmail.com>
2021-01-19 12:27:12 -0600

    book test done

--- borrower/borrower_test.go
+++ borrower/borrower_test.go
@@ -4,25 +4,30 @@ import (
 	"testing"
 )
 
-var jsonStringBr = "{\"name\":\"Borrower11\",\"maxBooks\":11}"
-var wantS = String(NewBorrower("Borrower1", 11))
-var wantS2 = String(NewBorrower("Borrower11", 1))
+//var br, _ = JsonStringToBorrower("{\"name\":\"Borrower1\",\"maxBooks\":1}")
+var badName, _ = JsonStringToBorrower("{\"name\":\"Borrower1X\",\"maxBooks\":1}")
 
-func TestSetBorrowerValues(t *testing.T) {
-	badBr, _ := JsonStringToBorrower(jsonStringBr)
-	n := "Borrower1"
-	gotBrS := String(SetName(badBr, n))
-	if gotBrS != wantS {
-		t.Fatalf("SetName(%v, %v) == %v, want %v", badBr, n, gotBrS, wantS)
-	}
-	mb := 1
-	gotBrS2 := String(SetMaxBooks(badBr, mb))
-	if gotBrS2 != wantS2 {
-		t.Fatalf("SetMaxBooks(%v) == %v, want %v", mb, gotBrS2, wantS2)
-	}
-	nb := NewBorrower("Borrower11", 11)
-	gotBrString, _ := BrToJsonString(nb)
-	if gotBrString != jsonStringBr {
-		t.Fatalf("BrToJsonString(%v) == %v, want %v", nb, gotBrString, jsonStringBr)
+//var badMB, _ = JsonStringToBorrower("{\"name\":\"Borrower1\",\"maxBooks\":19}")
+var wantBr = "Borrower1 (1 books)"
+
+func TestSetName(t *testing.T) {
+	name := "Borrower1"
+	gotBrN := String(SetName(badName, name))
+	if gotBrN != wantBr {
+		t.Fatalf("SetName(%v, %v) == %v, want %v", badName, name, gotBrN, wantBr)
 	}
 }
+
+//func TestSetBorrowerValues(t *testing.T) {
+//
+//	mb := 1
+//	gotBrS2 := String(SetMaxBooks(badBr, mb))
+//	if gotBrS2 != wantS2 {
+//		t.Fatalf("SetMaxBooks(%v) == %v, want %v", mb, gotBrS2, wantS2)
+//	}
+//	nb := NewBorrower("Borrower11", 11)
+//	gotBrString, _ := BrToJsonString(nb)
+//	if gotBrString != jsonStringBr {
+//		t.Fatalf("BrToJsonString(%v) == %v, want %v", nb, gotBrString, jsonStringBr)
+//	}
+//}
