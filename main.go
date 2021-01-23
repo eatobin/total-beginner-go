6586bb9002b163f21e89d3612bc25800868b9835
Eric Tobin <eatobin@gmail.com>
2021-01-23 17:23:06 -0600

    main progress

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"eatobin.com/totalbeginnergo/book"
 	"eatobin.com/totalbeginnergo/borrower"
 	"eatobin.com/totalbeginnergo/library"
+	"fmt"
 )
 
 func main() {
@@ -14,6 +15,10 @@ func main() {
 	borrowers = library.AddBorrower(borrowers, borrower.NewBorrower("Sue", 3))
 	books = library.AddBook(books, book.NewBook("War And Peace", "Tolstoy"))
 	books = library.AddBook(books, book.NewBook("Great Expectations", "Dickens"))
-	println("\nJust created new library")
-	println(library.StatusToString(books, borrowers))
+	fmt.Println("\nJust created new library")
+	fmt.Println(library.StatusToString(books, borrowers))
+
+	fmt.Println("Check out War And Peace to Sue")
+	books = library.CheckOut("Sue", "War And Peace", borrowers, books)
+	fmt.Println(library.StatusToString(books, borrowers))
 }
