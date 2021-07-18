18beb21ceaf64baf60bc48aad8da33d1d9e4d12e
Eric Tobin <eatobin@gmail.com>
2021-07-18 13:48:03 -0500

    borrower tests

--- borrower/borrower.go
+++ borrower/borrower.go
@@ -5,31 +5,31 @@ import (
 	"fmt"
 )
 
-// A Borrower has a Name and a max books
+// A Borrower has a Name and a MaxBooks
 type Borrower struct {
 	Name     string `json:"name"`
 	MaxBooks int    `json:"maxBooks"`
 }
 
-// NewBorrower needs a Name and a max books to create
-func NewBorrower(name string, maxBooks int) Borrower {
-	return Borrower{Name: name, MaxBooks: maxBooks}
-}
+//// NewBorrower needs a Name and a MaxBooks to create
+//func NewBorrower(name string, maxBooks int) Borrower {
+//	return Borrower{Name: name, MaxBooks: maxBooks}
+//}
 
 // SetName sets a Name for a Borrower
-func SetName(br Borrower, name string) Borrower {
+func (br Borrower) SetName(name string) Borrower {
 	br.Name = name
 	return br
 }
 
-// SetMaxBooks sets a max books for a Borrower
-func SetMaxBooks(br Borrower, maxBooks int) Borrower {
+//SetMaxBooks sets a MaxBooks for a Borrower
+func (br Borrower) SetMaxBooks(maxBooks int) Borrower {
 	br.MaxBooks = maxBooks
 	return br
 }
 
 // String makes a Borrower into a string
-func String(br Borrower) string {
+func (br Borrower) String() string {
 	return fmt.Sprintf("%s (%d books)", br.Name, br.MaxBooks)
 }
 
@@ -41,7 +41,7 @@ func JsonStringToBorrower(borrowerString string) (Borrower, error) {
 }
 
 // BrToJsonString turns a Borrower into a Borrower JSON string
-func BrToJsonString(borrower Borrower) (string, error) {
-	borrowerByte, err := json.Marshal(borrower)
+func (br Borrower) BrToJsonString() (string, error) {
+	borrowerByte, err := json.Marshal(br)
 	return string(borrowerByte), err
 }
