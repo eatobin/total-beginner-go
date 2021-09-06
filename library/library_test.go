333de006834a6a93f7407c1cabb9991bf498e013
Eric Tobin <eatobin@gmail.com>
2021-09-05 19:40:22 -0500

    library progress - really great!

--- library/library_test.go
+++ library/library_test.go
@@ -34,7 +34,7 @@ var jsonStringBooksBadParse = `[{"title""Title2","author":"Author22","borrower":
 var jsonStringBooksBadTitleField = `[{"noTitle":"Title2","author":"Author22","borrower":{"name":"NoName","maxBooks":-1}},{"title":"Title99","author":"Author99","borrower":{"name":"Borrower1","maxBooks":1}}]`
 var jsonStringBooksBadBorrowerField = `[{"title":"Title1","author":"Author1","borrower":{"noName":"Borrower1","maxBooks":1}},{"title":"Title2","author":"Author2","borrower":{"name":"Borrower2","maxBooks":2}}]`
 
-//var ss = "\n--- Status Report of Test Library ---\n\nTest Library: 3 books; 3 borrowers.\n\nTitle1 by Author1; Checked out to Borrower1\nTitle2 by Author2; Available\nTitle3 by Author3; Checked out to Borrower3\n\nBorrower1 [1 books]\nBorrower2 [2 books]\nBorrower3 [3 books]\n\n--- End of Status Report ---\n"
+var ss = "\n--- Status Report of Test Library ---\n\nTest Library: 3 books; 3 borrowers.\n\nTitle1 by Author1; Checked out to Borrower1\nTitle2 by Author2; Available\nTitle3 by Author3; Checked out to Borrower3\n\nBorrower1 [1 books]\nBorrower2 [2 books]\nBorrower3 [3 books]\n\n--- End of Status Report ---\n"
 
 func TestAddBorrower(t *testing.T) {
 	cases := []struct {
@@ -246,41 +246,41 @@ func Test_jsonStringToBooksFail(t *testing.T) {
 	}
 }
 
-//func TestBorrowersToJSONString(t *testing.T) {
-//	got, err := BorrowersToJSONSting(brs1)
-//	want := jsonStringBorrowers
-//	if got != want || err != nil {
-//		t.Errorf("BorrwersToJSONSting(%v) ==\n(%q) want \n(%q)",
-//			brs1, got, want)
-//	}
-//}
-//
-//func TestBooksToJSONString(t *testing.T) {
-//	got, err := BooksToJSONSting(bks1)
-//	want := jsonStringBooks
-//	if got != want || err != nil {
-//		t.Errorf("BooksToJSONSting(%v) ==\n(%q) want \n(%q)",
-//			bks1, got, want)
-//	}
-//}
-//
-//func TestStatusToString(t *testing.T) {
-//	br1libL := borrower.Borrower{Name: "Borrower1", MaxBooks: 1}
-//	br2libL := borrower.Borrower{Name: "Borrower2", MaxBooks: 2}
-//	br3libL := borrower.Borrower{Name: "Borrower3", MaxBooks: 3}
-//	brs2L := []borrower.Borrower{br1libL, br2libL, br3libL}
-//
-//	bk1libL := book.Book{Title: "Title1", Author: "Author1", Borrower: &br1lib}
-//	bk2libL := book.Book{Title: "Title2", Author: "Author2"}
-//	bk3libL := book.Book{Title: "Title3", Author: "Author3", Borrower: &br3lib}
-//	bks2L := []book.Book{bk1libL, bk2libL, bk3libL}
-//
-//	bks := bks2L
-//	brs := brs2L
-//	got := StatusToString(bks, brs)
-//	want := ss
-//	if got != want {
-//		t.Errorf("StatusToString(%v, %v) ==\n(%q) want \n(%q)",
-//			bks, brs, got, want)
-//	}
-//}
+func TestBorrowersToJSONString(t *testing.T) {
+	got, err := BorrowersToJSONSting(brs1)
+	want := jsonStringBorrowers
+	if got != want || err != nil {
+		t.Errorf("BorrwersToJSONSting(%v) ==\n(%q) want \n(%q)",
+			brs1, got, want)
+	}
+}
+
+func TestBooksToJSONString(t *testing.T) {
+	got, err := BooksToJSONSting(bks1)
+	want := jsonStringBooks
+	if got != want || err != nil {
+		t.Errorf("BooksToJSONSting(%v) ==\n(%q) want \n(%q)",
+			bks1, got, want)
+	}
+}
+
+func TestStatusToString(t *testing.T) {
+	br1libL := borrower.Borrower{Name: "Borrower1", MaxBooks: 1}
+	br2libL := borrower.Borrower{Name: "Borrower2", MaxBooks: 2}
+	br3libL := borrower.Borrower{Name: "Borrower3", MaxBooks: 3}
+	brs2L := []BorrowerPtr{&br1libL, &br2libL, &br3libL}
+
+	bk1libL := book.Book{Title: "Title1", Author: "Author1", Borrower: &br1lib}
+	bk2libL := book.Book{Title: "Title2", Author: "Author2"}
+	bk3libL := book.Book{Title: "Title3", Author: "Author3", Borrower: &br3lib}
+	bks2L := []BookPtr{&bk1libL, &bk2libL, &bk3libL}
+
+	bks := bks2L
+	brs := brs2L
+	got := StatusToString(bks, brs)
+	want := ss
+	if got != want {
+		t.Errorf("StatusToString(%v, %v) ==\n(%q) want \n(%q)",
+			bks, brs, got, want)
+	}
+}
