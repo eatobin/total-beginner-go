9bb9f6f848cb4f32d586c37a7040ef90357ffa8c
Eric Tobin <eatobin@gmail.com>
2021-09-28 14:08:45 -0500

    byValue book

--- book/book.go
+++ book/book.go
@@ -1,59 +1,59 @@
 package book
 
-//import (
-//	"encoding/json"
-//	"fmt"
-//	"github.com/eatobin/totalbeginnergo/borrower"
-//)
-//
-//type Borrower = borrower.Borrower
-//
-//// A Book has a Title and an Author - and maybe a Borrower
-//type Book struct {
-//	Title    string    `json:"title"`
-//	Author   string    `json:"author"`
-//	Borrower *Borrower `json:"borrower,omitempty"`
-//}
-//
-//// JsonStringToBook turns a Book JSON string into a Book
-//func JsonStringToBook(bookString string) (Book, error) {
-//	var book Book
-//	err := json.Unmarshal([]byte(bookString), &book)
-//	return book, err
-//}
-//
-//// SetTitle sets a Title for a Book
-//func (bk Book) SetTitle(title string) Book {
-//	bk.Title = title
-//	return bk
-//}
-//
-//// SetAuthor sets a Author for a Book
-//func (bk Book) SetAuthor(author string) Book {
-//	bk.Author = author
-//	return bk
-//}
-//
-//// SetBorrower takes a Borrower pointer and sets it for a Book
-//func (bk Book) SetBorrower(borrower *Borrower) Book {
-//	bk.Borrower = borrower
-//	return bk
-//}
-//
-//func (bk Book) availableString() string {
-//	if bk.Borrower == nil {
-//		return "Available"
-//	}
-//	return fmt.Sprintf("Checked out to %s", bk.Borrower.Name)
-//}
-//
-//// String makes a description of a Book
-//func (bk Book) String() string {
-//	return fmt.Sprintf("%s by %s; %s", bk.Title, bk.Author, Book.availableString(bk))
-//}
-//
-//// BkToJsonString turns a Book into a Book JSON string
-//func (bk Book) BkToJsonString() (string, error) {
-//	bookByte, err := json.Marshal(bk)
-//	return string(bookByte), err
-//}
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/eatobin/totalbeginnergo/borrower"
+)
+
+type Borrower = borrower.Borrower
+
+// A Book has a Title and an Author - and maybe a Borrower
+type Book struct {
+	Title    string    `json:"title"`
+	Author   string    `json:"author"`
+	Borrower *Borrower `json:"borrower,omitempty"`
+}
+
+// JsonStringToBook turns a Book JSON string into a Book
+func JsonStringToBook(bookString string) (Book, error) {
+	var book Book
+	err := json.Unmarshal([]byte(bookString), &book)
+	return book, err
+}
+
+// SetTitle sets a Title for a Book
+func (bk Book) SetTitle(title string) Book {
+	bk.Title = title
+	return bk
+}
+
+// SetAuthor sets a Author for a Book
+func (bk Book) SetAuthor(author string) Book {
+	bk.Author = author
+	return bk
+}
+
+// SetBorrower takes a Borrower pointer and sets it for a Book
+func (bk Book) SetBorrower(borrower *Borrower) Book {
+	bk.Borrower = borrower
+	return bk
+}
+
+func (bk Book) availableString() string {
+	if bk.Borrower == nil {
+		return "Available"
+	}
+	return fmt.Sprintf("Checked out to %s", bk.Borrower.Name)
+}
+
+// String makes a description of a Book
+func (bk Book) String() string {
+	return fmt.Sprintf("%s by %s; %s", bk.Title, bk.Author, Book.availableString(bk))
+}
+
+// BkToJsonString turns a Book into a Book JSON string
+func (bk Book) BkToJsonString() (string, error) {
+	bookByte, err := json.Marshal(bk)
+	return string(bookByte), err
+}
--- book/book_test.go
+++ book/book_test.go
@@ -1,28 +1,26 @@
 package book
 
-//import (
-//	"testing"
-//)
-//
-//var badTitle, _ = JsonStringToBook("{\"title\":\"Title1X\",\"author\":\"Author1\"}")
-//var bk1, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\"}")
-//var bk2, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}")
-//var bk3, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower1\",\"maxBooks\":1}}")
-//var wantAvail = "Title1 by Author1; Available"
-//var wantCheckedOut = "Title1 by Author1; Checked out to Borrower1"
-//
-//var newBorrower = Borrower{Name: "Borrower1", MaxBooks: 1}
-//var badAuthor, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1X\"}")
-//var jsonString = "{\"title\":\"Title1X\",\"author\":\"Author1\"}"
-//
-//func TestSetTitle(t *testing.T) {
-//	title := "Title1"
-//	gotBkT := badTitle.SetTitle(title).String()
-//	if gotBkT != wantAvail {
-//		t.Fatalf("(%v) SetTitle(%v) == %v, want %v", badTitle, title, gotBkT, wantAvail)
-//	}
-//}
-//
+import "testing"
+
+var badTitle, _ = JsonStringToBook("{\"title\":\"Title1X\",\"author\":\"Author1\"}")
+var bk1, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\"}")
+var bk2, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}")
+var bk3, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower1\",\"maxBooks\":1}}")
+var wantAvail = "Title1 by Author1; Available"
+var wantCheckedOut = "Title1 by Author1; Checked out to Borrower1"
+
+var newBorrower = Borrower{Name: "Borrower1", MaxBooks: 1}
+var badAuthor, _ = JsonStringToBook("{\"title\":\"Title1\",\"author\":\"Author1X\"}")
+var jsonString = "{\"title\":\"Title1X\",\"author\":\"Author1\"}"
+
+func TestSetTitle(t *testing.T) {
+	title := "Title1"
+	gotBkT := badTitle.SetTitle(title).String()
+	if gotBkT != wantAvail {
+		t.Fatalf("(%v) SetTitle(%v) == %v, want %v", badTitle, title, gotBkT, wantAvail)
+	}
+}
+
 //func TestSetAuthor(t *testing.T) {
 //	author := "Author1"
 //	gotBkA := badAuthor.SetAuthor(author).String()
